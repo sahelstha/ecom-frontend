dc21bf47b371a94938ac6d282dec2a5110039d6a
Sahel <sahelstha2061@gmail.com>
2026-04-20 19:04:14 +0545

    Fetching Products From the API

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productReducer } from "./reducers/ProductReducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { product: productReducer },
   preloadedState: {},
 });
 
