4b4a8363f8a96403350a623a6dd7ecdde63b20f4
Sahel <sahelstha2061@gmail.com>
2026-04-23 18:41:31 +0545

    fix: make the filter work in the url in dynamic way

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -6,9 +6,10 @@ import {
   Select,
   Tooltip,
 } from "@mui/material";
-import { useState } from "react";
-import { FaArrowUp, FaSearch } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { FaArrowDown, FaArrowUp, FaSearch } from "react-icons/fa";
 import { FiRefreshCcw, FiRefreshCw } from "react-icons/fi";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 const Filter = () => {
   const categories = [
@@ -19,10 +20,64 @@ const Filter = () => {
     { categoryId: 5, categoryName: "Toys" },
   ];
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathName = useLocation().pathname;
+  const navigate = useNavigate();
+
   const [category, setCategory] = useState("all");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const currentCategory = searchParams.get("category") || "all";
+    const currentSortOrder = searchParams.get("sortby") || "asc";
+    const currentSearchTerm = searchParams.get("keyword") || "";
+
+    setCategory(currentCategory);
+    setSortOrder(currentSortOrder);
+    setSearchTerm(currentSearchTerm);
+  }, [searchParams]);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (searchTerm) {
+        searchParams.set("keyword", searchTerm);
+      } else {
+        searchParams.delete("keyword");
+      }
+      navigate(`${pathName}?${searchParams.toString()}`);
+    }, 700);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchTerm, navigate, pathName]);
 
   const handleCategoryChange = (event) => {
+    const selectedCategory = event.target.value;
+    if (selectedCategory === "all") {
+      params.delete("category");
+    } else {
+      params.set("category", selectedCategory);
+    }
+
+    navigate(`${pathName}?${params}`);
+
     setCategory(event.target.value);
+  };
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => {
+      const newOrder = prevOrder === "asc" ? "desc" : "asc";
+      params.set("sortby", newOrder);
+      navigate(`${pathName}?${params}`);
+      return newOrder;
+    });
+  };
+
+  const handleClearFilter = () => {
+    navigate({ pathName: window.location.pathname });
   };
 
   return (
@@ -32,6 +87,8 @@ const Filter = () => {
         <input
           type="text"
           placeholder="Search Products"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className="border-gray-400 border text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline-none focus:ring-2 focus:ring-[#1976d2]"
         />
 
@@ -63,18 +120,22 @@ const Filter = () => {
         </FormControl>
 
         {/* SORT BUTTON AND CLEAR FILTER */}
-        <Tooltip title="Sort by price: asc">
+        <Tooltip title={`Sort by price: ${sortOrder}`}>
           <Button
             variant="contained"
             color="primary"
             className="flex items-center gap-2 h-10"
+            onClick={toggleSortOrder}
           >
             Sort By
-            <FaArrowUp />
+            {sortOrder === "asc" ? <FaArrowUp /> : <FaArrowDown />}
           </Button>
         </Tooltip>
 
-        <button className="flex items-center gap-2 bg-rose-800 text-white px-3 py-2  rounded-md transition duration-300 ease-in shadow-md focus:outline-none">
+        <button
+          className="flex items-center gap-2 bg-rose-800 text-white px-3 py-2  rounded-md transition duration-300 ease-in shadow-md focus:outline-none"
+          onClick={handleClearFilter}
+        >
           <FiRefreshCcw className="font-semibold size={16}" />
           <span className="font-semibold">Clear Filter</span>
         </button>
