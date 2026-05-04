2957d088201405865717b4713ff50900c807c026
Sahel <sahelstha2061@gmail.com>
2026-05-04 16:22:32 +0545

    add: layout for checkout page and add the step from mui

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Cart from "./components/cart/Cart";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import Register from "./components/auth/Register";
+import Checkout from "./components/checkout/Checkout";
 
 function App() {
   return (
@@ -24,6 +25,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+
           <Route path="/" element={<PrivateRoute publicPage />}>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
